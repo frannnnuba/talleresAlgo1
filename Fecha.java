fc3308dfca07e0724c0013eb265f1f2374e923be
frannnnuba <164945866+frannnnuba@users.noreply.github.com>
2024-05-06 20:15:19 -0300

    Add files via upload

--- Fecha.java
+++ Fecha.java
@@ -1,14 +1,16 @@
 package aed;
 
 public class Fecha {
+    public int dia = 0;
+    public int mes =0;
     public Fecha(int dia, int mes) {
-        public int dia;
-        public int mes;
+         this.dia = dia;
+        this.mes = mes;
     }
 
     public Fecha(Fecha fecha) {
-        this.dia = dia;
-        this.mes = mes;
+        this.dia  = fecha.dia;
+        this.mes = fech.mes;
     }
 
     public Integer dia() {
@@ -20,20 +22,34 @@ public class Fecha {
     }
     @Override
     public String toString() {
-        fecha.toString() = fecha.dia"/"fecha.mes;
+        fecha.toString() = fecha.dia + "/" + fecha.mes;
     }
 
     @Override
     public boolean equals(Object otra) {
-        if (otra = Fecha) {
-            
+        if (otra.getClass() == fecha.getClass()) {
+            if (otra.dia == this.dia && otra.mes = this.mes) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }else{
+            return false;
         }
-
-        ;
     }
 
     public void incrementarDia() {
-    ;
+    if (this.dia == 31 || this.dia == 28 && mes != 11){     //es una lista? preguntar para saber si asi hacer mes-1 = 11 para diciembre
+        this.dia = 1;
+        this.mes = this.mes +1; 
+    }elseif (this.dia == 31 & mes == 11){
+        this.dia = 1;
+        this.mes = 0;
+    }elseif(this.dia == 30 && this.mes != 3 && this.mes != 5 && this.mes != 7 && this.mes != 9 ){
+        this.dia = 31;
+        this.mes = this.mes +1;
+    }
     }
 
     private int diasEnMes(int mes) {
