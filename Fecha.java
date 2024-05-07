8a6f772979535d41a4f5dfd5f2db1296dd7daa53
frannnnuba <164945866+frannnnuba@users.noreply.github.com>
2024-05-07 13:43:59 -0300

    Add files via upload

--- Fecha.java
+++ Fecha.java
@@ -10,7 +10,7 @@ public class Fecha {
 
     public Fecha(Fecha fecha) {
         this.dia  = fecha.dia;
-        this.mes = fech.mes;
+        this.mes = fecha.mes;
     }
 
     public Integer dia() {
@@ -20,36 +20,47 @@ public class Fecha {
     public Integer mes() {
         return this.mes;
     }
-    @Override
-    public String toString() {
-        fecha.toString() = fecha.dia + "/" + fecha.mes;
-    }
+     @Override
+     public String toString() {
+         return this.dia + "/"+ this.mes;
+     }
 
-    @Override
-    public boolean equals(Object otra) {
-        if (otra.getClass() == fecha.getClass()) {
-            if (otra.dia == this.dia && otra.mes = this.mes) {
-                return true;
-            }
-            else {
-                return false;
-            }
-        }else{
-            return false;
-        }
-    }
+    // @Override
+    // public boolean equals(Object otra) {
+    //     if (otra.getClass() == this.getClass()) {
+    //         if ((otra.dia == this.dia) && (otra.mes == this.mes)) {
+    //             return true;
+    //         }
+    //         else {
+    //             return false;
+    //         }
+    //     }else{
+    //         return false;
+    //     }
+    // }
 
     public void incrementarDia() {
-    if (this.dia == 31 || this.dia == 28 && mes != 11){     //es una lista? preguntar para saber si asi hacer mes-1 = 11 para diciembre
-        this.dia = 1;
-        this.mes = this.mes +1; 
-    }elseif (this.dia == 31 & mes == 11){
-        this.dia = 1;
-        this.mes = 0;
-    }elseif(this.dia == 30 && this.mes != 3 && this.mes != 5 && this.mes != 7 && this.mes != 9 ){
-        this.dia = 31;
-        this.mes = this.mes +1;
-    }
+        int cantDias = diasEnMes(mes);
+        if (this.dia < cantDias){
+            this.dia = this.dia +1;
+        } else{
+            this.dia = 1;
+            if (this.mes != 12) {
+                this.mes = this.mes +1;
+            }else{
+                this.mes =1;
+            }
+        }
+    // if (this.dia == 31 || this.dia == 28 && mes != 11){     //es una lista? preguntar para saber si asi hacer mes-1 = 11 para diciembre
+    //     this.dia = 1;
+    //     this.mes = this.mes +1; 
+    // }elseif (this.dia == 31 & mes == 11){
+    //     this.dia = 1;
+    //     this.mes = 0;
+    // }elseif(this.dia == 30 && this.mes != 3 && this.mes != 5 && this.mes != 7 && this.mes != 9 ){
+    //     this.dia = 31;
+    //     this.mes = this.mes +1;
+    // }
     }
 
     private int diasEnMes(int mes) {
