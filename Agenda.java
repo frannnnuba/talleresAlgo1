572b57fc88df75e8266a31bfdbc1277e7e475cfe
frannnnuba <164945866+frannnnuba@users.noreply.github.com>
2024-05-10 21:53:45 -0300

    Add files via upload

--- Agenda.java
+++ Agenda.java
@@ -3,8 +3,9 @@ package aed;
 public class Agenda {
     Fecha fechaAct = null;
     Recordatorio record = null;
-    Recordatorio recordatorios[] = null;
+    Recordatorio[]  recordatorios= null;
     int longitud = 0;
+    ArregloRedimensionableDeRecordatorios arreglo = null;
 
     public Agenda(Fecha fechaActual) {
        this.fechaAct = new Fecha(fechaActual);
@@ -15,23 +16,25 @@ public class Agenda {
     }
 
      public void agregarRecordatorio(Recordatorio recordatorio) {
-        Recordatorio record = new Recordatorio(this.recordatorio().mensaje(), this); 
-        record.recordatorios = agregarAtras(recordatorio); 
+        // Recordatorio record = new Recordatorio(this.recordatorio().mensaje(), this); 
+        this.arreglo.agregarAtras(recordatorio); 
      }
 
-    // @Override
-    // public String toString() {
-    //     ;
+     @Override
+     public String toString() {
+         String palabra = null; 
+        for (int i = 0; i < arreglo.longitud(); i++ ){
+            palabra = this.arreglo.recordatorios[i].fecha().toString() + "/" + this.arreglo.recordatorios[i].fecha().dia() + "\n"
+                        .concat("=====\n")
+                        .concat("Clase Algoritmos @" + " " +  this.arreglo.recordatorios[i].fecha().toString() + " " + this.arreglo.recordatorios[i].horario().toString() + "\n")  
+                        .concat("Labo Algoritmos @" + this.arreglo.recordatorios[i].fecha.toString() + " " + this.arreglo.recordatorios[i].horario().toString() + "\n");
+        }
+        return palabra;
+     }
 
-    // }
+     public void incrementarDia() {
+         this.fechaAct.incrementarDia();
 
-    // public void incrementarDia() {
-    //     ;
-
-    // }
-
-    // public Fecha fechaActual() {
-    //     ;
-    // }
+     }
 
 }
